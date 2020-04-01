c45e1f4213a1a32a25e590d06bffbface4f76212
Natworpong Loyswai <Natworpong.LS@mail.kmutt.ac.th>
2020-04-02 00:42:54 +0700

    [UPDATE] API
    + From covid19uncle to covid19openapi

--- covid19.py
+++ covid19.py
@@ -1,16 +1,19 @@
-from covid19uncle import ThaiCovid19
+from covid19openapi import ThaiCovid19
+
 
 class ThaiPatient(object):
-  """Status for covid-19 patients in Thailand"""
-  def __new__(cls):
-    thai = ThaiCovid19()
-    patient = {
-      'update' : thai.get('อัพเดต'),
-      'cumulative' : thai.get('ผู้ป่วยสะสม'),
-      'new' : thai.get('ผู้ป่วยรายใหม่'),
-      'severe' : thai.get('ผู้ป่วยรุนแรง'),
-      'dead' : thai.get('ผู้ป่วยเสียชีวิต'),
-      'goHome' : thai.get('ผู้ป่วยกลับบ้านแล้ว'),
-      'references' : thai.get('อ้างอิง')
-    }
-    return patient
+    """Status for covid-19 patients in Thailand"""
+
+    def __new__(cls):
+        thai = ThaiCovid19()
+        today = thai.get('today')
+        patient = {
+            'update': today.get('UpdateDate'),
+            'cumulative': today.get('Confirmed'),
+            'new': today.get('NewConfirmed'),
+            'hospitalized': today.get('Hospitalized'),
+            'dead': today.get('Deaths'),
+            'goHome': today.get('Recovered'),
+            'references': today.get('Source')
+        }
+        return patient
